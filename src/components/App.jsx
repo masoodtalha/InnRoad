56580569e9f2118ad7c848dce0cb01a18b5389be
Talha Masood <masoodtalha7@gmail.com>
2018-07-11 22:30:25 +0500

    ADDED: basic UI with cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Task from './Task';
 
 class App extends Component {
 
   render() {
     return (
-      <div style={{margin: '5px'}}>
-        
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              InnRoad
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <br /> <br />
+        <Task />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,25 +4,11 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import {Router, Route} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
-// import { firebaseApp } from './firebase';
-// import {logUser} from './actions';
 import reducer from './reducers';
 import App from './components/App';
 
 const history = createBrowserHistory(); //Router V4 modification
 const store = createStore(reducer);
-// Authentication Listener
-// firebaseApp.auth().onAuthStateChanged((user) => {
-//   if(user){
-//     console.log("User has signed in or up",user);
-//     const { email } = user;
-//     store.dispatch(logUser(email)); //Since unlike components we can't bind action creator function in absence of class and state
-//     history.push("/app"); //If user looged in redirect to app
-//   }else {
-//     console.log("User has signed out or still need to sign in");
-//     history.replace('/signin');
-//   }
-// });
 
 ReactDOM.render(
   <Provider store={store}>
